a89f6ea7f961641b4d66a22e5b1c83818ba5f84d
fenglz77 <46338848+fenglz77@users.noreply.github.com>
2026-08-22 23:16:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,15 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.53/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# OpenClash 自动下载 Clash Meta 内核
+cd package/luci-app-openclash
+bash ./root/etc/openclash/core/clash_meta_auto.sh
+cd ../../
